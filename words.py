0748014138b8cd36971749505766aa0ac84e604d
Tristan <tristan.vujovic@gmail.com>
2018-09-04 12:13:41 +0200

    First commit, using nltk instead of enchant to be able to host on heroku

--- words.py
+++ words.py
@@ -1,12 +1,23 @@
 import itertools
-import enchant
+from nltk.corpus import words as nltk_words
+def is_english_word(word,dictionary):
+  # creation of this dictionary would be done outside of
+  #     the function because you only need to do it once.
+
+  try:
+    x = dictionary[word]
+    return True
+  except KeyError:
+    return False
+
 def word_finder(letters):
   words = {}
-  d = enchant.Dict("en_US")
   i=len(letters)
+  dictionary = dict.fromkeys(nltk_words.words(), None)
   while i>1:
     for p in itertools.permutations(letters,i):
-      if d.check(''.join(p)) and ''.join(p) not in[ x for v in words.values() for x in v]:
+      #print((''.join(p)).lower(), " ",is_english_word((''.join(p)).lower())
+      if is_english_word(''.join(p).lower(),dictionary) and ''.join(p) not in[ x for v in words.values() for x in v]:
         words.setdefault(str(i),[]).append(''.join(p))
     i-=1
   return words
